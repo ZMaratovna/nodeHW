67e75b39de3e7b7594920961ace03fb4d2c78b1c
Zhanna Kaymedenova <zh.kaymedenova@gmail.com>
2021-02-08 12:02:20 +0300

    added lost import

--- express-mongo/config/bearerStrategy.js
+++ express-mongo/config/bearerStrategy.js
@@ -1,5 +1,7 @@
 const BearerStrategy = require('passport-http-bearer').Strategy;
 
+const { User } = require('../models/User');
+
 const bearerStrategy = new BearerStrategy((jwt, done) => {
   User.findOne({ jwt }, (err, user) => {
     if (err) { return done(err); }
